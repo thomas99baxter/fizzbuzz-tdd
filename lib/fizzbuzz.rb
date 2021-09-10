a085dd943d8ce4e4fb4dcb1c0d0b7635cf71d250
Sam Thoburn <samthoburn@Sams-MacBook-Air.local>
2021-09-10 12:13:30 +0100

    Added tests for unexpected inputs and updated fizzbuzz method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,21 @@
+# Check that input is numeric
+def is_number?(obj)
+  obj.to_s == obj.to_i.to_s
+end
+
 def fizzbuzz(num)
-  if num == 3
-    return "fizz"
-  elsif num == 5
-    return "buzz"
-  elsif num == 15
-    return 'fizzbuzz'
+  if num == 0 || !is_number?(num)
+    return "error"
   else
-    # ensure return type stays as string
-    return num.to_s
+    if num % 15 == 0
+      return "fizzbuzz"
+    elsif num % 3 == 0
+      return "fizz"
+    elsif num % 5 == 0
+      return "buzz"
+    else
+      # ensure return type stays as string
+      return num.to_s
+    end
   end
 end
